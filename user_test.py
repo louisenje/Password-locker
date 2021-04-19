b5e18133c111ddf7a66eb217571c81736f84048c
louisenje <louisenje@gmail.com>
2021-04-19 17:28:16 +0300

    Fourth test to delete users from an user list

--- user_test.py
+++ user_test.py
@@ -44,7 +44,16 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
 # **********************************Fourth test to delete users from an user list...****************************************************
-    
+    def test_delete_contact(self):
+            '''
+            test_delete_contact to test if we can remove a contact from our contact list
+            '''
+            self.new_user.save_user()
+            test_user = User("Loui","123") # new contact
+            test_user.save_user()
+
+            self.new_user.delete_contact()# Deleting a contact object
+            self.assertEqual(len(User.user_list),1)
 
 # **********************************Fifth test to find a user from user list...****************************************************
     
